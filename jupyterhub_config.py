7e191ca4c834ddb0b405baf751a518c0637e5640
Jon Ludlam <jon@recoil.org>
2019-09-03 15:11:27 +0100

    A few changes to make running it locally easier
    Signed-off-by: Jon Ludlam <jon@recoil.org>

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -11,7 +11,7 @@ class VolumeCreatingSpawner(DockerSpawner):
     """
 
     def start(self):
-        directory = "/home/caelum/" + self.user.name
+        directory = "/users/" + self.user.name
         if not os.path.exists(directory):
             os.makedirs(directory)
             os.chown(directory,1000,1000)
@@ -53,7 +53,7 @@ notebook_dir = os.environ.get('DOCKER_NOTEBOOK_DIR') or '/home/opam/pkp'
 c.DockerSpawner.notebook_dir = notebook_dir
 # Mount the real user's Docker volume on the host to the notebook user's
 # notebook directory in the container
-c.DockerSpawner.volumes = { '/home/caelum/{username}': (notebook_dir + '/work') }
+c.DockerSpawner.volumes = { '/Users/jon/users/{username}': (notebook_dir + '/work') }
 # volume_driver is no longer a keyword argument to create_container()
 # c.DockerSpawner.extra_create_kwargs.update({ 'volume_driver': 'local' })
 # Remove containers once they are stopped
@@ -68,14 +68,15 @@ c.JupyterHub.hub_ip = 'jupyterhub'
 c.JupyterHub.hub_port = 8080
 
 # TLS config
-c.JupyterHub.port = 443
-c.JupyterHub.ssl_key = os.environ['SSL_KEY']
-c.JupyterHub.ssl_cert = os.environ['SSL_CERT']
+c.JupyterHub.port = 8000
+#c.JupyterHub.ssl_key = os.environ['SSL_KEY']
+#c.JupyterHub.ssl_cert = os.environ['SSL_CERT']
 
 # Choose an authentication method: github or raven
 
 # Authenticate users with GitHub OAuth
-c.JupyterHub.authenticator_class = 'oauthenticator.GitHubOAuthenticator'
+c.JupyterHub.authenticator_class = 'dummyauthenticator.DummyAuthenticator'
+c.DummyAuthenticator.password = "password"
 c.GitHubOAuthenticator.oauth_callback_url = os.environ['OAUTH_CALLBACK_URL']
 
 # Authenticate users with Raven
@@ -85,7 +86,7 @@ c.GitHubOAuthenticator.oauth_callback_url = os.environ['OAUTH_CALLBACK_URL']
 #c.RavenAuthenticator.login_logo="/opt/conda/lib/python3.6/site-packages/raven_auth/files/origami-camel.png"
 #c.RavenAuthenticator.ssl=True
 
-c.ConfigurableHTTPProxy.command = ['configurable-http-proxy', '--redirect-port', '80']
+#c.ConfigurableHTTPProxy.command = ['configurable-http-proxy', '--redirect-port', '80']
 
 # Persist hub data on volume mounted inside container
 data_dir = os.environ.get('DATA_VOLUME_CONTAINER', '/data')
@@ -100,7 +101,7 @@ c.JupyterHub.db_url = 'postgresql://postgres:{password}@{host}/{db}'.format(
 )
 
 # Whitlelist users and admins
-c.Authenticator.admin_users = admin = set(['jonludlam','ctk21','ghennequin'])
+c.Authenticator.admin_users = admin = set(['jon'])
 c.Authenticator.whitelist = whitelist = set()
 c.Authenticator.github_organization_whitelist = set(['ocamllabs','owlbarn','tarides','ocaml','pkp-neuro'])
 #pwd = os.path.dirname(__file__)
